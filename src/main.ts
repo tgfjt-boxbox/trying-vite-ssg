ffe5661e3e508475779254c22946e02c9735664d
Takashi Fujita <tgfjt.mail@gmail.com>
2023-09-08 11:23:05 +0900

    .

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import '@fontsource-variable/m-plus-2';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+AOS.init();
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// see also: https://tailwindcss.com/docs/guides/vite
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
